5f3033a067972d766d28226fea33dd21b929eb2b
Syahrul Safarudin Hasan <syahrul.safarudinh9@gmail.com>
2024-11-28 11:29:02 +0700

    initial init & setup db connection at application.properties

---
+++ src/main/java/com/syahrul_ssh/learn_java/LearnJavaApplication.java
@@ -0,0 +1,13 @@
+package com.syahrul_ssh.learn_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syahrul_ssh/learn_java/LearnJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syahrul_ssh.learn_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
